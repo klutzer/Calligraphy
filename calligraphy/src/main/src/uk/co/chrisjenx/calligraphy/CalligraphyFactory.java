24ad620789004c80b6febee3da63a65c3b1ac4c2
klutzer <erico.klutzer@gmail.com>
2015-06-14 19:30:40 -0300

    support.v7 ajustado

--- calligraphy/src/main/src/uk/co/chrisjenx/calligraphy/CalligraphyFactory.java
+++ calligraphy/src/main/src/uk/co/chrisjenx/calligraphy/CalligraphyFactory.java
@@ -10,7 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.widget.TextView;
-import android.widget.Toolbar;
+import android.support.v7.widget.Toolbar;
 
 class CalligraphyFactory {
 
